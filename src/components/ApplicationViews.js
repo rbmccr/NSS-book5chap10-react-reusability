55f1541d86397daa9a4c8877848509262b77f16a
Brendan McCray <rbmccr@gmail.com>
2018-11-19 11:06:35 -0600

    completed chapter 4 practice exercise, need to do challenge

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -5,38 +5,27 @@ import LocationList from './location/LocationList'
 import EmployeeList from './employee/EmployeeList'
 import OwnerList from './owner/OwnerList'
 
-
 class ApplicationViews extends Component {
-  employeesFromAPI = [
-    { id: 1, name: "Jessica Younker" },
-    { id: 2, name: "Jordan Nelson" },
-    { id: 3, name: "Zoe LeBlanc" },
-    { id: 4, name: "Blaise Roberts" }
-  ]
 
-  locationsFromAPI = [
-    { id: 1, name: "Nashville North", address: "500 Circle Way" },
-    { id: 2, name: "Nashville South", address: "10101 Binary Court" }
-  ]
+  componentDidMount() {
+    const newState = {}
 
-  animalsFromAPI = [
-    { id: 1, name: "Doodles" },
-    { id: 2, name: "Jack" },
-    { id: 3, name: "Angus" },
-    { id: 4, name: "Henley" },
-    { id: 5, name: "Derkins" },
-    { id: 6, name: "Checkers" }
-  ]
+    fetch("http://localhost:5002/animals")
+      .then(r => r.json())
+      .then(animals => newState.animals = animals)
+      .then(() => fetch("http://localhost:5002/employees")
+        .then(r => r.json()))
+      .then(employees => newState.employees = employees)
+      .then(() => fetch("http://localhost:5002/locations")
+        .then(r => r.json()))
+      .then(locations => newState.locations = locations)
+      .then(() => fetch("http://localhost:5002/owners")
+        .then(r => r.json()))
+      .then(owners => newState.owners = owners)
+      .then(() => this.setState(newState))
+  }
 
-  ownersFromAPI = [
-    { id: 1, name: "Ryan Tanay" },
-    { id: 2, name: "Emma Beaton" },
-    { id: 3, name: "Dani Adkins" },
-    { id: 4, name: "Adam Oswalt" },
-    { id: 5, name: "Fletcher Bangs" },
-    { id: 6, name: "Angela Lee" }
-  ]
-
+  //join table
   animalsOwned = [
     { id: 1, animalId: 1, ownerId: 1 },
     { id: 2, animalId: 1, ownerId: 2 },
@@ -48,10 +37,10 @@ class ApplicationViews extends Component {
   ]
 
   state = {
-    owners: this.ownersFromAPI,
-    employees: this.employeesFromAPI,
-    locations: this.locationsFromAPI,
-    animals: this.animalsFromAPI
+    owners: [],
+    locations: [],
+    animals: [],
+    employees: []
   }
 
   render() {
